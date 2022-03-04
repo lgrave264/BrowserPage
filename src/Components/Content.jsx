c12f3fbd389d53ac86355d697eac902591eed4c6
lgrave264 <71471845+lgrave264@users.noreply.github.com>
2022-03-04 14:53:55 -0700

    fixed background image

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,8 +1,36 @@
+import placeholder from '../Util/favicon.png';
+
 const Content = () => {
 
 	return(
-		<div id="linked">
-			<div>hi</div>
+		<div>
+			<img id="logo" alt="Logo" src={placeholder}/>
+			<div id="linked">
+				<div className="Link">
+					hi1
+				</div>
+				<div className="Link">
+					hi2
+				</div>
+				<div className="Link">
+					hi3
+				</div>
+				<div className="Link">
+					hi4
+				</div>
+				<div className="Link">
+					hi5
+				</div>
+				<div className="Link">
+					hi6
+				</div>
+				<div className="Link">
+					hi7
+				</div>
+				<div className="Link">
+					hi8
+				</div>
+			</div>
 		</div>
 	)
 }
